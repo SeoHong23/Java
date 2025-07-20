9fde29f87a97606d5d4c09e796eac1b60160b923
SeoHong23 <ghd2408@naver.com>
2025-07-20 20:14:43 +0900

    test123

---
+++ thisisjava/src/ch17/sec11/Student2.java
@@ -0,0 +1,15 @@
+package ch17.sec11;
+
+public class Student2 {
+	private String name;
+	private int score;
+	
+	public Student2(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+	
+	public String getName() { return name; }
+	public int getScore() { return score; }
+
+}
