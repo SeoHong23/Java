3e49495add1b1fc6238328ad2a056195b66012a4
SeoHong23 <ghd2408@naver.com>
2025-09-10 17:49:38 +0900

    oracle example

---
+++ thisisjava/src/ch20/oracle/sec09/exam01/User.java
@@ -0,0 +1,16 @@
+package ch20.oracle.sec09.exam01;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class User {
+	private String userId;
+	private String userName;
+	private String userPassword;
+	private int userAge;
+	private String userEmail;
+	
+
+}
